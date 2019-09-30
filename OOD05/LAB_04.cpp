0d839dddd31d815bf91a64f1da60a85b68d1b979
noelleter <shdmsrla@gmail.com>
2019-09-30 09:34:26 +0900

    DES

--- OOD05/LAB_04.cpp
+++ OOD05/LAB_04.cpp
@@ -10,6 +10,10 @@ public:
     Animal(){
         this -> numberOfLeg = numberOfLeg;
     }
+     
+     아예 아무 생성자도 없으면 기본 생성자 생기는데
+     아래 매개변수 하나짜리 있어서 기본 생성자 내가 만들어야 함
+     
     */
     Animal(int numberOfLeg){
         this -> numberOfLeg = numberOfLeg;
@@ -25,3 +29,4 @@ int main(){
     
     return 0;
 }
+
