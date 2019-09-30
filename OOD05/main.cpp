90e45c29d7b1fc5222e9f3ca9a14b094d54b8e7b
noelleter <shdmsrla@gmail.com>
2019-09-30 09:18:45 +0900

    LAB 01, 02

--- OOD05/main.cpp
+++ OOD05/main.cpp
@@ -7,9 +7,46 @@
 //
 
 #include <iostream>
+using namespace std;
+int i;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// i 값이 3 나옴
+class A{
+public:
+    ~A(){
+        i = 10;
+    }
+};
+
+int foo(){
+    i = 3;
+    A ob;
+    return i;
+}
+
+int main() {
+    cout << foo() << endl;
     return 0;
 }
+
+/*
+ 2. i 값이 10 출력되도록 하는 것 ( 레퍼런스 활용 )
+ 
+ class A{
+ public:
+ ~A(){
+ i = 10;
+ }
+ };
+ 
+ int& foo(){
+ i = 3;
+ A ob;
+ return i;
+ }
+ 
+ int main() {
+ cout << foo() << endl;
+ return 0;
+ }
+ */
